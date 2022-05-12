7c2ce6f76243612ace2fb24b65babfc7770d87e0
khaled elaadwy <46924819+kElaadwy@users.noreply.github.com>
2022-05-12 20:07:10 +0200

    UI-optimization | contact us page

--- AFNews/Controllers/HomeController.cs
+++ AFNews/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using AFNews.Models;
+﻿using AFNews.Data;
+using AFNews.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,22 +7,55 @@ namespace AFNews.Controllers
 {
     public class HomeController : Controller
     {
+
+        NewsDbContext db;
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, NewsDbContext context)
         {
             _logger = logger;
+            db = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var categories = db.Categories.ToList();
+            return View(categories);
         }
 
         public IActionResult Privacy()
         {
             return View();
         }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SaveContact(ContactUs contact)
+        {
+            db.Contacts.Add(contact);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Messages()
+        {
+            return View(db.Contacts.ToList());
+        }
+
+        public IActionResult News(int id)
+        {
+            var news = db.News.Where(x => x.categoryId == id).ToList();
+
+            return View(news);
+        }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
